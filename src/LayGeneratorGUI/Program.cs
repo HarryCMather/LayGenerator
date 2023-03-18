5dd2ea44151c3d86eec6ff6a81a806a9ec66b04c
HarryCMather <harrycmather@gmail.com>
2023-03-18 21:54:57 +0000

    ci: Use Windows build agents, update version stri

--- src/LayGeneratorGUI/Program.cs
+++ src/LayGeneratorGUI/Program.cs
@@ -10,8 +10,15 @@ internal static class Program
     [STAThread]
     private static void Main()
     {
-        string version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
-        version ??= "Development";
+        string version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? " Development Build";
+
+        if (version.Count(character => character == '.') == 3)
+        {
+            version = version[..^2];
+        }
+
+        version = version == "1.0.0" ? " Development Build" : $@" V-{version}";
+
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1(version));
     }
